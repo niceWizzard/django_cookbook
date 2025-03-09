7235762062b8f3f42ceaf6639060cf55894522c6
RIchard Manansala <richard232manansala@gmail.com>
2025-03-09 20:01:56 +0800

    Added media_url

--- project/settings.py
+++ project/settings.py
@@ -139,6 +139,12 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT =  BASE_DIR / "staticfiles"
 
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
