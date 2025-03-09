17b5538c73bc123357e6779d72b306793412dfab
RIchard Manansala <richard232manansala@gmail.com>
2025-03-09 18:39:07 +0800

    Added linux check for npm_bin_path

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import platform
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,6 +139,8 @@ STATIC_ROOT =  BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 NPM_BIN_PATH = 'npm.cmd'
+if platform.system() == "Linux":
+    NPM_BIN_PATH = "~/nodejs/bin/npm"
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
