321ee8794533902fa4035b54c4da392d96806af1
RIchard Manansala <richard232manansala@gmail.com>
2025-03-09 17:04:55 +0800

    Added homepage

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tailwind',
     'theme',
+    'project',
 ]
 
 MIDDLEWARE = [
--- project/views.py
+++ project/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def homepage(req):
-    return render(req, 'layout.html')
+    return render(req, 'home.html')
