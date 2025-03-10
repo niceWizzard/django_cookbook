eca55494725e6f3e1479de39307469e4e71cf9aa
RIchard Manansala <richard232manansala@gmail.com>
2025-03-10 19:18:01 +0800

    Added Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Recipe(models.Model):
+    image = models.ImageField(blank=True,null=True)
+    title = models.CharField(max_length=72)
+    tags = models.CharField(max_length=256)
+    recipe = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
